2e9a2fb1a4cc2a40be6d0bbba0f8fe64dd69a990
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-02-11 00:29:14 +0100

    demands schema done

--- models/demand.js
+++ models/demand.js
@@ -1,7 +1,161 @@
-// subcategory
-// id professionnel
-// id user
-// address
-// time slot
-// date
-// totalFee
+const mongoose = require("mongoose");
+const Joi = require("joi"); // this is a class
+
+var Schema = mongoose.Schema;
+
+const demandsSchema = new Schema({
+  subcategory: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 200
+      },
+      description: {
+        type: String,
+        required: true,
+        minlength: 20,
+        maxlength: 5000
+      },
+      category: {
+        type: new mongoose.Schema({
+          name: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 200
+          },
+          description: {
+            type: String,
+            required: true,
+            minlength: 20,
+            maxlength: 5000
+          },
+          urlImage: {
+            type: String,
+            required: false
+          }
+        }),
+        required: true
+      },
+      urlImage: {
+        type: String,
+        required: false
+      }
+    }),
+    required: true
+  },
+  professional: {
+    type: new mongoose.Schema({}),
+    required: true
+  },
+  user: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 200
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+        unique: true
+      },
+      isAdmin: Boolean,
+      isProfessionnel: Boolean,
+      urlImage: {
+        type: String,
+        required: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+        unique: true
+      },
+      address: {
+        type: String,
+        required: false,
+        minlength: 5,
+        maxlength: 1000
+      }
+    }),
+    required: true
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1000
+  },
+  totalFee: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "In progress",
+    minlength: 5,
+    maxlength: 1000
+  },
+  startDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  totalHours: {
+    type: Number,
+    required: true
+  }
+});
+
+const Demand = mongoose.model("Demand", demandsSchema);
+
+demandsSchema.statics.lookup = function(subcategoryId) {
+  return this.findOne({
+    "subcategory._id": subcategoryId
+  });
+};
+
+demandsSchema.statics.lookup = function(userId) {
+  return this.findOne({
+    "user._id": userId
+  });
+};
+
+demandsSchema.statics.lookup = function(professionalId) {
+  return this.findOne({
+    "professional._id": professionalId
+  });
+};
+
+function validateDemand(demand) {
+  const schema = {
+    startDate: Joi.date().required(),
+    endDate: Joi.date().required(),
+    professionalId: Joi.objectId().required(),
+    userId: Joi.objectId().required(),
+    subcategoryId: Joi.objectId().required(),
+    totalFee: Joi.number().required(),
+    totalHours: Joi.number().required(),
+    address: Joi.string()
+      .min(5)
+      .max(1000)
+  };
+  return Joi.validate(demand, schema);
+}
+
+module.exports.Demand = Demand;
+module.exports.validateDemand = validateDemand;
+module.exports.demandsSchema = demandsSchema;
--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const categories = require("../routes/categories");
 const subcategories = require("../routes/subcategories");
+const demands = require("../routes/demands");
 const home = require("../routes/home");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
@@ -14,6 +15,7 @@ module.exports = function(app) {
   app.use("/api/categories", categories);
   app.use("/api/subcategories", subcategories);
   app.use("/api/professionals", professionals);
+  app.use("/api/demands", demands);
   app.use("/", home);
   app.use(error);
 };
